9c2e7d9a3d59065f485948ff0c92f7f8c257f8bd
William L. Anderson <band@acm.org>
2024-02-09 15:27:59 -0600

    scrub_path fix for issue #54

--- mwb.py
+++ mwb.py
@@ -64,11 +64,10 @@ def load_config(path):
     with open(path) as infile:
         return yaml.safe_load(infile)
 
-# scrub wiki path to handle ' ', '_', '?', '"', '#' characters in wiki page names
-# change ' ', ?', and '#' to '_', because they're inconvenient in URLs
-# change double quote to '_' because of problems in HTML links
+# scrub wiki path to handle ' ', '_', '?', '"', '#', '%' characters in wiki page names
+# change those characters to '_' to avoid URL generation errors
 def scrub_path(filepath):
-    return re.sub(r'([ _?\#"]+)', '_', filepath)
+    return re.sub(r'([ _?\#%"]+)', '_', filepath)
 
 # find outgoing wikilinks in a wiki page
 def find_tolinks(file):
