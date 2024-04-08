af46aa413b33d79eb4b44444040499e3d7eaffd7
Willliam L. Anderson <band@users.noreply.github.com>
2024-04-08 15:43:38 -0500

    capturing logging output; run test without a subprocess

--- tests/bespoke-tests/test_runmwb.py
+++ tests/bespoke-tests/test_runmwb.py
@@ -1,31 +1,30 @@
-import sys
 import pytest
+import sys
+import logging
 from io import StringIO
 
-# Import your foo.py module here
+# import run MWB module
 import run_mwb
 
-# Fixture to capture the stdout
-@pytest.fixture
-def capture_stderr(monkeypatch):
-    buffer = StringIO()
-    monkeypatch.setattr(sys, 'stderr', buffer)
-    yield buffer
-    buffer.close()
-
 # Test cases
-def test_mwb_with_valid_args(capture_stderr):
+def test_mwb_with_valid_args():
+    log_capture_string = StringIO()
+    stream_handler = logging.StreamHandler(log_capture_string)
+    logger = logging.getLogger()
+    logger.addHandler(stream_handler)
+    logger.setLevel(logging.INFO)
+
     # Set the sys.argv to mimic command-line arguments
     sys.argv = ['run_mwb.py', '-i', 'test-input', '-b', 'baseline']
     
     # Call the main function of your foo.py program
     run_mwb.main()
     
-    # Get the captured stderr
-    captured_output = capture_stderr.getvalue()
+    # Get the captured logging output
+    captured_output = log_capture_string.getvalue().strip()
     
     # Assert the expected output
-    expected_output = "args: Namespace(input='test-input', baseline='baseline', random=False, force=None, mwb_output='test-output', mwb_config='input-dir/.massivewikibuilder/mwb.yaml', mwb_templates='input-dir/.massivewikibuilder/this-wiki-themes/basso')\nRunning mwb.py...\nmwb.py executed successfully.\nComparing directories...\nComparison finished, no faults.\n"
+    expected_output = "args: Namespace(input='test-input', baseline='baseline', random=False, force=None, mwb_output='test-output', mwb_config='test-input/.massivewikibuilder/mwb.yaml', mwb_templates='test-input/.massivewikibuilder/this-wiki-themes/basso')\nRunning mwb.py...\nmwb.py executed successfully.\nComparing directories...\nComparison finished, no faults."
     
     assert captured_output == expected_output
 
