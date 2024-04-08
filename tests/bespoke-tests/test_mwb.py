091ed34c51e9cc4cab2d04632eafc20773eb973d
Willliam L. Anderson <band@users.noreply.github.com>
2024-04-08 11:20:29 -0500

    clean up after test-input/ replacement

--- tests/bespoke-tests/test_mwb.py
+++ tests/bespoke-tests/test_mwb.py
@@ -5,7 +5,7 @@ import pytest
 
 def test_mwb():
     # run bespoke-test_mwb.py with specific arguments and check if the output is as expected
-    expected_output = "INFO: args: Namespace(input='test-input', baseline='baseline', random=False, force=None, mwb_output='test-output', mwb_config='input-dir/.massivewikibuilder/mwb.yaml', mwb_templates='input-dir/.massivewikibuilder/this-wiki-themes/basso')\nINFO: Running mwb.py...\nINFO: mwb.py executed successfully.\nINFO: Comparing directories...\nINFO: Comparison finished, no faults."
+    expected_output = "INFO: args: Namespace(input='test-input', baseline='baseline', random=False, force=None, mwb_output='test-output', mwb_config='test-input/.massivewikibuilder/mwb.yaml', mwb_templates='test-input/.massivewikibuilder/this-wiki-themes/basso')\nINFO: Running mwb.py...\nINFO: mwb.py executed successfully.\nINFO: Comparing directories...\nINFO: Comparison finished, no faults."
     
     completed_process = subprocess.run(["python", "bespoke-test_mwb.py", "-i", "test-input", "-b", "baseline"], capture_output=True, text=True)
     result = completed_process.stderr.strip()
