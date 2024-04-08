666a6265479e03982030d06083316fce8574cc36
Willliam L. Anderson <band@users.noreply.github.com>
2024-04-08 15:49:26 -0500

    cleanup and copyedits

--- tests/bespoke-tests/test_runmwb.py
+++ tests/bespoke-tests/test_runmwb.py
@@ -1,29 +1,29 @@
 import pytest
 import sys
+import io
 import logging
-from io import StringIO
 
-# import run MWB module
+# import the run MWB module
 import run_mwb
 
-# Test cases
+# test cases
 def test_mwb_with_valid_args():
-    log_capture_string = StringIO()
+    log_capture_string = io.StringIO()
     stream_handler = logging.StreamHandler(log_capture_string)
     logger = logging.getLogger()
     logger.addHandler(stream_handler)
     logger.setLevel(logging.INFO)
 
-    # Set the sys.argv to mimic command-line arguments
+    # set sys.argv to mimic command-line arguments
     sys.argv = ['run_mwb.py', '-i', 'test-input', '-b', 'baseline']
     
-    # Call the main function of your foo.py program
+    # Call the main function of run_mwb.py program
     run_mwb.main()
     
-    # Get the captured logging output
+    # capture the logging output
     captured_output = log_capture_string.getvalue().strip()
     
-    # Assert the expected output
+    # assert captured and expected output equivalence
     expected_output = "args: Namespace(input='test-input', baseline='baseline', random=False, force=None, mwb_output='test-output', mwb_config='test-input/.massivewikibuilder/mwb.yaml', mwb_templates='test-input/.massivewikibuilder/this-wiki-themes/basso')\nRunning mwb.py...\nmwb.py executed successfully.\nComparing directories...\nComparison finished, no faults."
     
     assert captured_output == expected_output
