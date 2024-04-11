27b8c8b12877415861a2fff5a59409fdbc717190
Willliam L. Anderson <band@users.noreply.github.com>
2024-04-11 11:02:48 -0500

    do not need separate run_mwb() function

--- tests/bespoke-tests/test_mwb.py
+++ tests/bespoke-tests/test_mwb.py
@@ -4,14 +4,6 @@ import subprocess
 import pytest
 import filecmp
 
-def run_mwb():
-    # run bespoke-test_mwb.py with specific arguments and check if the output is as expected
-    expected_output = "INFO: args: Namespace(input='test-input', baseline='baseline', random=False, force=None, mwb_output='test-output', mwb_config='test-input/.massivewikibuilder/mwb.yaml', mwb_templates='test-input/.massivewikibuilder/this-wiki-themes/basso')\nINFO: Running mwb.py...\nINFO: mwb.py executed successfully.\nINFO: Comparing directories...\nINFO: Comparison finished, no faults."
-    
-    completed_process = subprocess.run(["python", "bespoke-test_mwb.py", "-i", "test-input", "-b", "baseline"], capture_output=True, text=True)
-    result = completed_process.stderr.strip()
-    assert result == expected_output
-
 def compare_mwb_directories(output, baseline):
     comparison = filecmp.dircmp(output, baseline)
     if comparison.left_only or comparison.right_only or not ('build-results.json' in comparison.diff_files and len(comparison.diff_files) == 1):
@@ -21,7 +13,8 @@ def compare_mwb_directories(output, baseline):
 
 @pytest.fixture(scope="module")
 def run_and_verify():
-    run_mwb()
+    # run bespoke-test_mwb.py with specific arguments and check return status
+    subprocess.run(["python", "bespoke-test_mwb.py", "-i", "test-input", "-b", "baseline"], check=True)
 
 def test_compare_output_with_baseline(run_and_verify):
     assert compare_mwb_directories("test-output/", "baseline/"), "Directory contents do not match."
